9b71498673f22424dd612868d4623073178684b6
Royglee <rosta94@gmail.com>
2016-12-15 21:46:45 +0100

    expire date

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var mqtt = require('mqtt');
 var currentValue = [];
 var lastSensorData = [];
 var mode  = 0; //0-manual 1-auto
+var AndroidSessionDuration = 60*1000;
 
 var consumeFeny = new Date().getTime();
 var tokens = [];
@@ -21,6 +22,7 @@ client.on('connect', function () {
     client.subscribe('Arduino/Sensor1');
     client.subscribe('Arduino/Beavatkozo0');
     client.subscribe('Arduino/Beavatkozo1');
+	
     client.subscribe('Arduino/Feny0');
     client.subscribe('Arduino/Feny1');
     client.subscribe('Arduino/Mode');
@@ -59,7 +61,7 @@ client.on('message', function (topic, message) {
     }
     if(topic=="Arduino/Feny1" || topic=="Arduino/Feny0"){
         var now = Date.now();
-        if(now-consumeFeny > 30){ //Ha az elõzõ általunk küldött üzenet óta eltelt legalább 30ms akkor dolgozzuk csak fel
+        if(now-consumeFeny > 50){ //Ha az elõzõ általunk küldött üzenet óta eltelt legalább 50ms akkor dolgozzuk csak fel
             //console.log(message.toString());
             var id=parseInt(topic.slice(-1));
             io.emit("Arduino/Feny", {value:message.toString(),id:id});
@@ -78,7 +80,7 @@ client.on('message', function (topic, message) {
 
         //A lejárt kulcsok kitörlése a tömbbõl
             for(var i = tokens.length-1; i>=0; i--) {
-                if(now - tokens[i][1] >  1000*60) {
+                if(now - tokens[i][1] >  AndroidSessionDuration) {
                     tokens.splice(i, 1);
                 }
             }
@@ -156,6 +158,7 @@ io.on('connection', function(socket){
 
     socket.on('Auth/Token', function(msg){
         var token = msg.toString();
+        var duration;
         socket['SWAuthToken'] = token;
 	//console.log(token);
 	//console.log(tokens);
@@ -163,7 +166,9 @@ io.on('connection', function(socket){
         if(!isTokenValid(token)){
             socket.emit('Auth/tokenStatus', "INVALID");
         } else{
-            socket.emit('Auth/tokenStatus', "VALID");
+            duration = getMillisUntilExpire(token).toString();
+            socket.emit('Auth/tokenStatus', duration);
+
         }
 
     });
@@ -177,13 +182,28 @@ function timeStamp() {
 
 function isTokenValid(token){
 	var isValid = false;
+    var now = Date.now();
     tokens.forEach(function(value, index){
-	console.log(value[0],token);
-        if(value[0].toString() == token.toString()) {
+	//console.log(value[0],token);
+        if(value[0].toString() == token.toString() && now-value[1] < AndroidSessionDuration) {
 		console.log("match");
 		isValid =  true;}
     });
     return isValid;
+}
+
+function getMillisUntilExpire(token){
+    var savedIndex = -1;
+    tokens.forEach(function(value, index){
+        if(value[0].toString() == token.toString()) {
+            savedIndex =  index;}
+    });
+    if (savedIndex > -1){
+        var duration = AndroidSessionDuration-(Date.now() - tokens[savedIndex][1]);
+        return duration;
+    } else{
+        return "INVALID"
+    }
 }
 
 http.listen(process.env.HTTP_LISTEN_PORT,process.env.HTTP_LISTEN_IP, function(){
